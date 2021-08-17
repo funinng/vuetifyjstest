b7f6ff3d03f2abda092975f77a18f6d6027d4ff8
fang <649392395@qq.com>
2021-08-17 14:30:04 +0800

    添加vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,14 @@
 <template>
-  <div>
-    <header-bar></header-bar>
-    <v-btn color="primary">da</v-btn>
-    <v-btn color="primary">ads</v-btn>
-    <footer-bar></footer-bar>
-  </div>
+<v-container>
+  <h2>hello word</h2>
+</v-container>
 </template>
 <script>
-import headerBar from "../components/header";
-import footerBar from "../components/footer";
+
 export default {
-  name: 'Home',
+
   components: {
-    footerBar,
-    headerBar
+
   }
 
 }
